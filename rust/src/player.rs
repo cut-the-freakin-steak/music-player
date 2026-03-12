c31dd10094fc421d815122171a374c7c702e2b37
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-03-11 18:12:13 -0600

    trying out hboxcontainers

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,15 +1,15 @@
-use godot::classes::{CheckButton, Control, IControl};
+use godot::classes::{CheckButton, HBoxContainer, IHBoxContainer};
 use godot::prelude::*;
 
 /// The music player scene
 #[derive(GodotClass)]
-#[class(init, base = Control)]
+#[class(init, base = HBoxContainer)]
 struct Player {
-    base: Base<Control>,
+    base: Base<HBoxContainer>,
 
     #[init(node = "PlayPause")]
     play_pause: OnReady<Gd<CheckButton>>,
 }
 
 #[godot_api]
-impl IControl for Player {}
+impl IHBoxContainer for Player {}
