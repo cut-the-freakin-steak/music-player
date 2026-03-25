7ce9191ad456488c129ca095b87d7f870e5f2efa
cut-the-freakin-steak <calvaradolopez@proton.me>
2026-03-24 18:16:56 -0600

    sigh.,,, lets try tokio.,,,

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,6 +3,17 @@ use godot::prelude::*;
 mod audio_singleton;
 mod player;
 
+// set up tokio
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+
+static TOKIO_RT: OnceLock<Runtime> = OnceLock::new();
+
+#[allow(clippy::missing_panics_doc)]
+pub fn runtime() -> &'static Runtime {
+    TOKIO_RT.get_or_init(|| Runtime::new().expect("Failed to create Tokio runtime"))
+}
+
 struct MyExtension;
 
 #[gdextension]
